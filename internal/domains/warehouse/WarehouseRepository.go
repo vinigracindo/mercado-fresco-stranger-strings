92ed0c446beba0201ae65fc153fc474666c86597
Gabriel Ferreira Silva <gabrielfssilva99@gmail.com>
2022-06-03 17:26:45 -0300

    feature: create interface service and create method

--- internal/domains/warehouse/WarehouseRepository.go
+++ internal/domains/warehouse/WarehouseRepository.go
@@ -1,27 +1,36 @@
 package warehouse
 
-var wh []WarehouseModel
+import (
+	"fmt"
+)
+
+var wh = make([]WarehouseModel, 100)
 var id int64 = 0
 
-type warehouse struct{}
+type repository struct{}
 
-type WarehouseInterface interface {
-	Create(wr *WarehouseModel) (WarehouseModel, error)
+type Repository interface {
+	Store(wr *WarehouseModel) (WarehouseModel, error)
 	createID() int64
 }
 
-func (w warehouse) createID() int64 {
+func newRepository() Repository {
+	return &repository{}
+}
+
+func (w repository) createID() int64 {
 	id += id + 1
 	return id
 }
 
-func (w warehouse) Create(wr *WarehouseModel) (WarehouseModel, error) {
-	wr.Id = w.createID()
-	wh = append(wh, *wr)
+func (w repository) Store(new *WarehouseModel) (WarehouseModel, error) {
+	for i := range wh {
+		if wh[i].WarehouseCode == new.WarehouseCode {
+			return WarehouseModel{}, fmt.Errorf("error: already a warehouse with the code: %s", new.WarehouseCode)
+		}
+	}
+	new.Id = w.createID()
+	wh = append(wh, *new)
 
-	return *wr, nil
-}
-
-func newRepository() WarehouseInterface {
-	return &warehouse{}
+	return *new, nil
 }
--- internal/domains/warehouse/WarehouseService.go
+++ internal/domains/warehouse/WarehouseService.go
@@ -1,1 +1,24 @@
 package warehouse
+
+type service struct {
+	repository Repository
+}
+type Service interface {
+	Create(wr *WarehouseModel) (WarehouseModel, error)
+}
+
+func newService(r Repository) Service {
+	return &service{
+		repository: r,
+	}
+}
+
+func (s service) Create(new *WarehouseModel) (WarehouseModel, error) {
+	wh, err := s.repository.Store(new)
+
+	if err != nil {
+		return WarehouseModel{}, err
+	}
+
+	return wh, nil
+}
