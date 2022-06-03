8addb4605cde91c859890e3ae870f9c35d4ab8f8
Katielly Feitosa <katiellyfeitosa@outlook.com>
2022-06-03 18:35:44 -0300

    create route getAll section

--- cmd/server/controllers/SectionController.go
+++ cmd/server/controllers/SectionController.go
@@ -1,1 +1,32 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vinigracindo/mercado-fresco-stranger-strings/internal/domains/section"
+)
+
+type Controller struct {
+	service section.Service
+}
+
+func NewSection(s section.Service) *Controller {
+	return &Controller{
+		service: s,
+	}
+}
+
+func (c *Controller) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		section, err := c.service.GetAll()
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, section)
+	}
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/vinigracindo/mercado-fresco-stranger-strings/cmd/server/controllers"
+	"github.com/vinigracindo/mercado-fresco-stranger-strings/internal/domains/section"
+)
 
 func main() {
+
 	router := gin.Default()
+
+	// Section routes
+	repositorySection := section.NewRepository()
+	serviceSection := section.NewService(repositorySection)
+	controllerSection := controllers.NewSection(serviceSection)
+
+	groupSection := router.Group("/sections")
+	groupSection.GET("/", controllerSection.GetAll())
 
 	router.Run()
 }
