55942d27110dc037657609b4e796be6b13a53e7d
Gabriel Ferreira Silva <gabrielfssilva99@gmail.com>
2022-06-03 15:47:02 -0300

    warehouse model created

--- internal/domains/warehouse/WarehouseModel.go
+++ internal/domains/warehouse/WarehouseModel.go
@@ -1,1 +1,10 @@
 package warehouse
+
+type Warehouse struct {
+	Id                 int64   `json:"id"`
+	Address            string  `json:"address"`
+	Telephone          string  `json:"telephone"`
+	WarehouseCode      string  `json:"warehouse_code"`
+	MinimunCapacity    int64   `json:"minimun_capacity"`
+	MinimunTemperature float64 `json:"minimun_temperature"`
+}
