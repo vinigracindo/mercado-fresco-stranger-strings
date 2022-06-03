283acb7e95c4d35b6625ef7e57ab9d723cd78972
Katielly Feitosa <katiellyfeitosa@outlook.com>
2022-06-03 18:35:57 -0300

    create route getAll section

--- internal/domains/section/SectionRepository.go
+++ internal/domains/section/SectionRepository.go
@@ -1,1 +1,18 @@
 package section
+
+var listSection []Section
+
+type Repository interface {
+	GetAll() ([]Section, error)
+}
+
+type repository struct{}
+
+func NewRepository() Repository {
+	return &repository{}
+}
+
+func (r *repository) GetAll() ([]Section, error) {
+	listSection = []Section{}
+	return listSection, nil
+}
--- internal/domains/section/SectionService.go
+++ internal/domains/section/SectionService.go
@@ -1,1 +1,23 @@
 package section
+
+type Service interface {
+	GetAll() ([]Section, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{
+		repository: r,
+	}
+}
+
+func (s *service) GetAll() ([]Section, error) {
+	listSection, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	return listSection, nil
+}
