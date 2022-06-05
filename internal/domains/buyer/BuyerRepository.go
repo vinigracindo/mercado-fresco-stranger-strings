8cb91dc3777f7d8865a70f7bfbc0cb2da86753ff
zurcje <jwdcruz@gmail.com>
2022-06-05 08:59:56 -0300

    create buyer service and repository

--- internal/domains/buyer/BuyerRepository.go
+++ internal/domains/buyer/BuyerRepository.go
@@ -1,1 +1,33 @@
 package buyer
+
+import "fmt"
+
+var buyers []Buyer = []Buyer{}
+
+type Repository interface {
+	Store(id int64, cardNumberId int64, firstName string, lastName string) (Buyer, error)
+}
+
+type repository struct {
+}
+
+func createId() int64 {
+	return int64(len(buyers) + 1)
+}
+
+func (repository) Store(id int64, cardNumberId int64, firstName string, lastName string) (Buyer, error) {
+
+	for _, buyer := range buyers {
+		if buyer.CardNumberId == cardNumberId {
+			return Buyer{}, fmt.Errorf("comprador já está cadastrado")
+		}
+	}
+	newBuyer := Buyer{createId(), cardNumberId, firstName, lastName}
+	buyers = append(buyers, newBuyer)
+
+	return newBuyer, nil
+}
+
+func NewRepository() Repository {
+	return &repository{}
+}
--- internal/domains/buyer/BuyerService.go
+++ internal/domains/buyer/BuyerService.go
@@ -1,1 +1,26 @@
 package buyer
+
+type Service interface {
+	Store(id int64, cardNumberId int64, firstName string, lastName string) (Buyer, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func (s service) Store(id int64, cardNumberId int64, firstName string, lastName string) (Buyer, error) {
+
+	buyer, err := s.repository.Store(id, cardNumberId, firstName, lastName)
+
+	if err != nil {
+		return Buyer{}, err
+	}
+
+	return buyer, nil
+}
+
+func NewService(r Repository) Service {
+	return &service{
+		repository: r,
+	}
+}
