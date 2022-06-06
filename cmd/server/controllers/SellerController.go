5bb1b7c6010d6c78a0bc6a865ca3da72452e7e5c
afonsocgl <afonsocgl@gmail.com>
2022-06-06 01:06:51 -0300

    Adding SellerController

--- cmd/server/controllers/SellerController.go
+++ cmd/server/controllers/SellerController.go
@@ -1,1 +1,34 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vinigracindo/mercado-fresco-stranger-strings/internal/domains/seller"
+)
+
+type SellerController struct {
+	service seller.Service
+}
+
+func NewSeller(s seller.Service) SellerController {
+	return SellerController{
+		service: s,
+	}
+}
+
+func (c SellerController) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		seller, err := c.service.GetAll()
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"data": seller,
+		})
+	}
+}
