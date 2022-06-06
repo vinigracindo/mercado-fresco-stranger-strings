c3dd03f0b528a6c7e204a2c05d0f7e843ef94375
zurcje <jwdcruz@gmail.com>
2022-06-06 14:10:44 -0300

    update 'buyer model' pattern

--- internal/domains/buyer/BuyerModel.go
+++ internal/domains/buyer/BuyerModel.go
@@ -2,7 +2,7 @@ package buyer
 
 type Buyer struct {
 	Id           int64  `json:"id"`
-	CardNumberId int64  `json:"cardNumberId"`
-	FirstName    string `json:"firstName"`
-	LastName     string `json:"lastName"`
+	CardNumberId int64  `json:"card_number_id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
 }
