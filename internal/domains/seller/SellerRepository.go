2a3d6f906e021ba8ff7f55f5ce569f5bc90dca6a
afonsocgl <afonsocgl@gmail.com>
2022-06-06 10:20:03 -0300

    Created repository from CreateSeller

--- internal/domains/seller/SellerRepository.go
+++ internal/domains/seller/SellerRepository.go
@@ -7,6 +7,7 @@ var listSeller []Seller
 type Repository interface {
 	GetAll() ([]Seller, error)
 	Get(id int64) (Seller, error)
+	CreateSeller(cid int64, companyName, address, telephone string) (Seller, error)
 }
 
 type repository struct{}
@@ -28,4 +29,22 @@ func (r *repository) Get(id int64) (Seller, error) {
 	}
 
 	return Seller{}, fmt.Errorf("seller id %d not found", id)
+}
+
+func (r *repository) CreateSeller(cid int64, companyName, address, telephone string) (Seller, error) {
+	nextId := seller[len(seller)-1].Id + 1
+	seller := Seller{
+		Id:          nextId,
+		CompanyName: companyName,
+		Address:     address,
+		Telephone:   telephone,
+	}
+
+	if !r.CidIsUnique(Cid) {
+		return Seller{}, fmt.Errorf("cid %s is alredy in use", cid)
+	}
+
+	listSeller = append(listSeller, seller)
+	return seller, nil
+
 }
--- internal/domains/seller/sellerService.go
+++ internal/domains/seller/sellerService.go
@@ -3,6 +3,7 @@ package seller
 type Service interface {
 	GetAll() ([]Seller, error)
 	Get(id int64) (Seller, error)
+	CreateSeller(cid int64, companyName, address, telephone string) (Seller, error)
 }
 
 type service struct {
@@ -26,6 +27,14 @@ func (s *service) GetAll() ([]Seller, error) {
 func (s *service) Get(id int64) (Seller, error) {
 	seller, err := s.repository.Get(id)
 
+	if err != nil {
+		return Seller{}, err
+	}
+	return seller, nil
+}
+
+func (s *service) CreateSeller(cid int64, companyName, address, telephone string) (Seller, error) {
+	seller, err := s.repository.CreateSeller(cid, companyName, address, telephone)(Seller, error)
 	if err != nil {
 		return Seller{}, err
 	}
