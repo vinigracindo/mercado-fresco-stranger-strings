2c2e9b8cf6f5926d602ced8f293035dba7633677
afonsocgl <afonsocgl@gmail.com>
2022-06-06 00:47:21 -0300

    adding SellerService and SellerRepository

--- internal/domains/seller/SellerRepository.go
+++ internal/domains/seller/SellerRepository.go
@@ -1,1 +1,18 @@
 package seller
+
+var listSeller []Seller
+
+type Repository interface {
+	GetAll() ([]Seller, error)
+}
+
+type repository struct{}
+
+func NewRepository() Repository {
+	return &repository{}
+}
+
+func (r *repository) GetAll() ([]Seller, error) {
+	listSeller = []Seller{}
+	return listSeller, nil
+}
--- internal/domains/seller/sellerService.go
+++ internal/domains/seller/sellerService.go
@@ -1,1 +1,23 @@
 package seller
+
+type Service interface {
+	GetAll() ([]Seller, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{
+		repository: r,
+	}
+}
+
+func (s *service) GetAll() ([]Seller, error) {
+	listSeller, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	return listSeller, nil
+}
