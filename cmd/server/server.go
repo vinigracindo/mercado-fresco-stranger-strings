68f7cef9218a7803c366e7f154662174221d3244
zurcje <jwdcruz@gmail.com>
2022-06-09 08:44:09 -0300

    add id patch route - buyer

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -100,7 +100,7 @@ func (api *APIServer) Run(port int) {
 	buyerGroup.GET("/", buyerController.GetAll())
 	buyerGroup.GET("/:id", buyerController.GetId())
 	buyerGroup.POST("/", buyerController.Store())
-	buyerGroup.PATCH("/", buyerController.Update())
+	buyerGroup.PATCH("/:id", buyerController.Update())
 	buyerGroup.DELETE("/:id", buyerController.Delete())
 
 	router.Run(fmt.Sprintf(":%d", port))
