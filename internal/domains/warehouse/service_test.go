13050116ed9652266db72e5040f88dd8462f16ca
Gabriel Ferreira Silva <gabrielfssilva99@gmail.com>
2022-06-17 14:41:01 -0300

    feature: add tests for GetByID method at WarehouseService

--- internal/domains/warehouse/service_test.go
+++ internal/domains/warehouse/service_test.go
@@ -82,3 +82,52 @@ func Test_Service_GetAll(t *testing.T) {
 		assert.Equal(t, expectedWarehouseList, resultList)
 	})
 }
+
+func Test_Service_GetByID(t *testing.T) {
+	expectedWarehouseList := []warehouse.WarehouseModel{
+		{
+			Id:                 0,
+			Address:            "Avenida Teste",
+			Telephone:          "31 999999999",
+			WarehouseCode:      "hg312",
+			MinimunCapacity:    1111111,
+			MinimunTemperature: 9999999,
+		},
+		{
+			Id:                 1,
+			Address:            "Avenida Teste Segunda",
+			Telephone:          "31 77777777",
+			WarehouseCode:      "od78",
+			MinimunCapacity:    5555555,
+			MinimunTemperature: 444444,
+		},
+	}
+
+	t.Run("find_by_id_existent: procura um warehouse pelo id valida e retornar", func(t *testing.T) {
+		repo := mocks.NewRepository(t)
+
+		repo.On("GetById", int64(1)).Return(expectedWarehouseList[1], nil)
+
+		service := warehouse.NewService(repo)
+
+		result, _ := service.GetById(int64(1))
+
+		assert.Equal(t, expectedWarehouseList[1], result)
+	})
+
+	t.Run("find_by_id_non_existent: procura um warehouse por um ID invalida e retonar um erro", func(t *testing.T) {
+
+		var fakeID int64 = 9999
+		errMsg := fmt.Errorf("erros: no warehouse was found with id %d", fakeID)
+
+		repo := mocks.NewRepository(t)
+
+		repo.On("GetById", int64(fakeID)).Return(warehouse.WarehouseModel{}, errMsg)
+
+		service := warehouse.NewService(repo)
+
+		_, err := service.GetById(int64(fakeID))
+
+		assert.Error(t, err)
+	})
+}
