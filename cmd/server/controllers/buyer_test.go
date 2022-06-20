3574e8dae9f16ccfb7f7c1bc3751057376baa72b
zurcje <jwdcruz@gmail.com>
2022-06-19 21:28:29 -0300

    create buyer tests - controller

--- cmd/server/controllers/buyer_test.go
+++ cmd/server/controllers/buyer_test.go
@@ -2,9 +2,17 @@ package controllers_test
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"net/http/httptest"
+	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert/v2"
+	"github.com/vinigracindo/mercado-fresco-stranger-strings/cmd/server/controllers"
+	"github.com/vinigracindo/mercado-fresco-stranger-strings/internal/domains/buyer"
+	"github.com/vinigracindo/mercado-fresco-stranger-strings/internal/domains/buyer/mocks"
 )
 
 func SetUpRouter() *gin.Engine {
@@ -20,4 +28,79 @@ func CreateRequestTest(gin *gin.Engine, method string, url string, body []byte) 
 	gin.ServeHTTP(response, request)
 
 	return response
+}
+
+func Test_Controller_Buyer_CreateBuyer(t *testing.T) {
+	expectBuyer := &buyer.Buyer{
+
+		Id:           0,
+		CardNumberId: 402323,
+		FirstName:    "FirstNameTest",
+		LastName:     "LastNameTest",
+	}
+
+	body := &buyer.Buyer{
+		Id:           0,
+		CardNumberId: 402323,
+		FirstName:    "FirstNameTest",
+		LastName:     "LastNameTest",
+	}
+
+	bodyFail := &buyer.Buyer{
+		Id:           0,
+		CardNumberId: 0,
+		FirstName:    "",
+		LastName:     "",
+	}
+
+	t.Run("create_ok", func(t *testing.T) {
+
+		service := mocks.NewService(t)
+
+		service.On("Create",
+			expectBuyer.CardNumberId,
+			expectBuyer.FirstName,
+			expectBuyer.LastName).Return(expectBuyer, nil)
+
+		controller := controllers.NewBuyer(service)
+		requestBody, _ := json.Marshal(body)
+
+		r := SetUpRouter()
+		r.POST("/api/v1/buyers", controller.Create())
+		response := CreateRequestTest(r, "POST", "/api/v1/buyers", requestBody)
+
+		assert.Equal(t, response.Code, http.StatusCreated)
+	})
+
+	t.Run("create_fail", func(t *testing.T) {
+
+		service := mocks.NewService(t)
+		controller := controllers.NewBuyer(service)
+		requestBody, _ := json.Marshal(bodyFail)
+
+		r := SetUpRouter()
+		r.POST("/api/v1/buyers", controller.Create())
+		response := CreateRequestTest(r, "POST", "/api/v1/buyers", requestBody)
+
+		assert.Equal(t, response.Code, http.StatusUnprocessableEntity)
+	})
+
+	t.Run("create_conflict", func(t *testing.T) {
+
+		service := mocks.NewService(t)
+
+		service.On("Create",
+			expectBuyer.CardNumberId,
+			expectBuyer.FirstName,
+			expectBuyer.LastName).Return(nil, fmt.Errorf("buyer already registered: %d", expectBuyer.CardNumberId))
+
+		controller := controllers.NewBuyer(service)
+		requestBody, _ := json.Marshal(body)
+
+		r := SetUpRouter()
+		r.POST("/api/v1/buyers", controller.Create())
+		response := CreateRequestTest(r, "POST", "/api/v1/buyers", requestBody)
+
+		assert.Equal(t, response.Code, http.StatusConflict)
+	})
 }
