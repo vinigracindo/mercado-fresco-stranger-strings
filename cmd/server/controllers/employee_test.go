159b863a8f65f2c38b873567368d94312fb114bb
Vinnicyus Gracindo <vinnicyus.gracindo@mercadolivre.com>
2022-06-20 14:55:28 -0300

    refactor employee conroller tests

--- cmd/server/controllers/employee_test.go
+++ cmd/server/controllers/employee_test.go
@@ -3,6 +3,7 @@ package controllers_test
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,6 +16,7 @@ import (
 )
 
 var (
+	ENDPOINT    = "/api/v1/employees"
 	invalidJSON = []byte(`{invalid json}`)
 )
 
@@ -26,6 +28,14 @@ func makeEmployee() employees.Employee {
 		LastName:     "Doe",
 		WarehouseId:  1,
 	}
+}
+
+func makeExpectedResponse(data any) string {
+	expectedOutput := map[string]any{
+		"data": data,
+	}
+	expectedResponseBody, _ := json.Marshal(expectedOutput)
+	return string(expectedResponseBody)
 }
 
 func SetUpRouter() *gin.Engine {
@@ -54,14 +64,14 @@ func TestEmployeeController_Create(t *testing.T) {
 		mockService.On("Create", "123456", "John", "Doe", int64(1)).Return(expectedEmployee, nil).Once()
 
 		reqBody, _ := json.Marshal(expectedEmployee)
-		response := ExecuteTestRequest(router, http.MethodPost, "/api/v1/employees", reqBody)
+		response := ExecuteTestRequest(router, http.MethodPost, ENDPOINT, reqBody)
 
 		assert.Equal(t, http.StatusCreated, response.Code)
 	})
 
 	t.Run("create_fail: Se o objeto JSON não contiver os campos necessários, um código 422 será	retornado.", func(t *testing.T) {
 		reqBody, _ := json.Marshal(invalidJSON)
-		response := ExecuteTestRequest(router, http.MethodPost, "/api/v1/employees", reqBody)
+		response := ExecuteTestRequest(router, http.MethodPost, ENDPOINT, reqBody)
 
 		assert.Equal(t, http.StatusUnprocessableEntity, response.Code)
 	})
@@ -69,7 +79,7 @@ func TestEmployeeController_Create(t *testing.T) {
 	t.Run("create_conflict: Se o card_number_id já existir, ele	retornará um erro 409 Conflict.", func(t *testing.T) {
 		mockService.On("Create", "123456", "John", "Doe", int64(1)).Return(employees.Employee{}, employees.ErrCardNumberMustBeUnique).Once()
 		reqBody, _ := json.Marshal(expectedEmployee)
-		response := ExecuteTestRequest(router, http.MethodPost, "/api/v1/employees", reqBody)
+		response := ExecuteTestRequest(router, http.MethodPost, ENDPOINT, reqBody)
 
 		assert.Equal(t, http.StatusConflict, response.Code)
 	})
@@ -89,15 +99,12 @@ func TestEmployeeController_GetAll(t *testing.T) {
 
 	t.Run("find_all: Quando a solicitação for bem-sucedida, o back-end retornará uma lista de todos os funcionários existentes.", func(t *testing.T) {
 		mockService.On("GetAll").Return(expectedEmployees, nil).Once()
-		response := ExecuteTestRequest(router, http.MethodGet, "/api/v1/employees", nil)
+		response := ExecuteTestRequest(router, http.MethodGet, ENDPOINT, nil)
 
-		expectedOutput := map[string]any{
-			"data": expectedEmployees,
-		}
-		expectedResponseBody, _ := json.Marshal(expectedOutput)
+		expectedResponseBody := makeExpectedResponse(expectedEmployees)
 
 		assert.Equal(t, http.StatusOK, response.Code)
-		assert.Contains(t, string(expectedResponseBody), response.Body.String())
+		assert.JSONEq(t, expectedResponseBody, response.Body.String())
 	})
 }
 
@@ -106,10 +113,11 @@ func TestEmployeeController_GetById(t *testing.T) {
 	controller := controllers.NewEmployee(mockService)
 	router := SetUpRouter()
 	router.GET("/api/v1/employees/:id", controller.GetById())
+	url := fmt.Sprintf("%s/%d", ENDPOINT, 1)
 
 	t.Run("find_by_id_non_existent: Quando o funcionário não existir, um código 404 será retornado.", func(t *testing.T) {
 		mockService.On("GetById", int64(1)).Return(nil, employees.ErrEmployeeNotFound).Once()
-		response := ExecuteTestRequest(router, http.MethodGet, "/api/v1/employees/1", nil)
+		response := ExecuteTestRequest(router, http.MethodGet, url, nil)
 
 		assert.Equal(t, http.StatusNotFound, response.Code)
 	})
@@ -117,13 +125,9 @@ func TestEmployeeController_GetById(t *testing.T) {
 	t.Run("find_by_id_existent: Quando a solicitação for bem-sucedida, o back-end retornará as informações solicitadas do funcionário", func(t *testing.T) {
 		expectedEmployee := makeEmployee()
 		mockService.On("GetById", int64(1)).Return(&expectedEmployee, nil).Once()
-		response := ExecuteTestRequest(router, http.MethodGet, "/api/v1/employees/1", nil)
+		response := ExecuteTestRequest(router, http.MethodGet, url, nil)
 
-		expectedOutput := map[string]any{
-			"data": expectedEmployee,
-		}
-
-		expectedResponseBody, _ := json.Marshal(expectedOutput)
+		expectedResponseBody := makeExpectedResponse(expectedEmployee)
 
 		assert.Equal(t, http.StatusOK, response.Code)
 		assert.JSONEq(t, string(expectedResponseBody), response.Body.String())
@@ -135,6 +139,7 @@ func TestEmployeeController_Update(t *testing.T) {
 	controller := controllers.NewEmployee(mockService)
 	router := SetUpRouter()
 	router.PATCH("/api/v1/employees/:id", controller.UpdateFullname())
+	url := fmt.Sprintf("%s/%d", ENDPOINT, 1)
 
 	t.Run("update_ok: Quando a atualização dos dados for bem sucedida, o funcionário será devolvido	com as informações atualizadas juntamente com um código 200",
 		func(t *testing.T) {
@@ -144,7 +149,7 @@ func TestEmployeeController_Update(t *testing.T) {
 
 			mockService.On("UpdateFullname", int64(1), "Jane", "Doe").Return(&expectedEmployee, nil).Once()
 			reqBody, _ := json.Marshal(expectedEmployee)
-			response := ExecuteTestRequest(router, http.MethodPatch, "/api/v1/employees/1", reqBody)
+			response := ExecuteTestRequest(router, http.MethodPatch, url, reqBody)
 
 			expectedOutput := map[string]any{
 				"data": expectedEmployee,
@@ -159,7 +164,7 @@ func TestEmployeeController_Update(t *testing.T) {
 	t.Run("update_non_existent: Se o funcionário a ser atualizado não existir, um código 404 será retornado.", func(t *testing.T) {
 		mockService.On("UpdateFullname", int64(1), "John", "Doe").Return(nil, employees.ErrEmployeeNotFound).Once()
 		reqBody, _ := json.Marshal(makeEmployee())
-		response := ExecuteTestRequest(router, http.MethodPatch, "/api/v1/employees/1", reqBody)
+		response := ExecuteTestRequest(router, http.MethodPatch, url, reqBody)
 
 		assert.Equal(t, http.StatusNotFound, response.Code)
 	})
@@ -170,17 +175,18 @@ func TestEmployeeController_Delete(t *testing.T) {
 	controller := controllers.NewEmployee(mockService)
 	router := SetUpRouter()
 	router.DELETE("/api/v1/employees/:id", controller.Delete())
+	url := fmt.Sprintf("%s/%d", ENDPOINT, 1)
 
 	t.Run("delete_non_existent: Quando o funcionário não existir, um código 404 será retornado.", func(t *testing.T) {
 		mockService.On("Delete", int64(1)).Return(employees.ErrEmployeeNotFound).Once()
-		response := ExecuteTestRequest(router, http.MethodDelete, "/api/v1/employees/1", nil)
+		response := ExecuteTestRequest(router, http.MethodDelete, url, nil)
 
 		assert.Equal(t, http.StatusNotFound, response.Code)
 	})
 
 	t.Run("delete_ok: Quando a exclusão for bem-sucedida, um código 204 será retornado.", func(t *testing.T) {
 		mockService.On("Delete", int64(1)).Return(nil).Once()
-		response := ExecuteTestRequest(router, http.MethodDelete, "/api/v1/employees/1", nil)
+		response := ExecuteTestRequest(router, http.MethodDelete, url, nil)
 
 		assert.Equal(t, http.StatusNoContent, response.Code)
 	})
