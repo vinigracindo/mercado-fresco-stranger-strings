233ee8cf55b1157f019a1d58a5802a93c54ab164
zurcje <jwdcruz@gmail.com>
2022-06-20 23:15:13 -0300

    buyer controller tests - refactored

--- cmd/server/controllers/buyer_test.go
+++ cmd/server/controllers/buyer_test.go
@@ -20,206 +20,236 @@ func SetUpRouter() *gin.Engine {
 	return router
 }
 
-func CreateRequestTest(gin *gin.Engine, method string, url string, body []byte) *httptest.ResponseRecorder {
+func CreateRequestTest(router *gin.Engine, method string, url string, body []byte) *httptest.ResponseRecorder {
 
 	request := httptest.NewRequest(method, url, bytes.NewBuffer(body))
 	response := httptest.NewRecorder()
 
-	gin.ServeHTTP(response, request)
+	router.ServeHTTP(response, request)
 
 	return response
 }
 
-func Test_Controller_Buyer_CreateBuyer(t *testing.T) {
-	expectBuyer := &buyer.Buyer{
+var ENDPOINT = "/api/v1/buyers"
 
-		Id:           0,
-		CardNumberId: 402323,
-		FirstName:    "FirstNameTest",
-		LastName:     "LastNameTest",
-	}
+var expectBuyer = &buyer.Buyer{
+	Id:           0,
+	CardNumberId: 402323,
+	FirstName:    "FirstNameTest",
+	LastName:     "LastNameTest",
+}
 
-	body := &buyer.Buyer{
-		Id:           0,
-		CardNumberId: 402323,
-		FirstName:    "FirstNameTest",
-		LastName:     "LastNameTest",
-	}
+var body = &buyer.Buyer{
+	CardNumberId: 402323,
+	FirstName:    "FirstNameTest",
+	LastName:     "LastNameTest",
+}
 
-	t.Run("create_ok", func(t *testing.T) {
+func Test_Controller_Create(t *testing.T) {
+	service := mocks.NewService(t)
 
-		service := mocks.NewService(t)
+	t.Run("create_ok: Quando a entrada de dados for bem-sucedida, um código 201 será retornado junto com o objeto inserido.", func(t *testing.T) {
 
 		service.On("Create",
 			expectBuyer.CardNumberId,
 			expectBuyer.FirstName,
-			expectBuyer.LastName).Return(expectBuyer, nil)
+			expectBuyer.LastName).Return(expectBuyer, nil).Once()
 
 		controller := controllers.NewBuyer(service)
 		requestBody, _ := json.Marshal(body)
 
 		r := SetUpRouter()
-		r.POST("/api/v1/buyers", controller.Create())
-		response := CreateRequestTest(r, "POST", "/api/v1/buyers", requestBody)
+		r.POST(ENDPOINT, controller.Create())
+		response := CreateRequestTest(r, http.MethodPost, ENDPOINT, requestBody)
 
-		assert.Equal(t, response.Code, http.StatusCreated)
+		assert.Equal(t, http.StatusCreated, response.Code)
+
 	})
 
-	t.Run("create_fail", func(t *testing.T) {
+	t.Run("create_fail: Se o objeto JSON não contiver os campos necessários, um código 422 será retornado.", func(t *testing.T) {
 
 		service := mocks.NewService(t)
 		controller := controllers.NewBuyer(service)
 
 		r := SetUpRouter()
-		r.POST("/api/v1/buyers", controller.Create())
-		response := CreateRequestTest(r, "POST", "/api/v1/buyers", []byte{})
+		r.POST(ENDPOINT, controller.Create())
+		response := CreateRequestTest(r, http.MethodPost, ENDPOINT, []byte{})
 
-		assert.Equal(t, response.Code, http.StatusUnprocessableEntity)
+		assert.Equal(t, http.StatusUnprocessableEntity, response.Code)
 	})
 
-	t.Run("create_conflict", func(t *testing.T) {
-
-		service := mocks.NewService(t)
+	t.Run("create_conflict: Se o card_number_id já existir, ele retornará um erro 409 Conflict.", func(t *testing.T) {
 
 		service.On("Create",
 			expectBuyer.CardNumberId,
 			expectBuyer.FirstName,
-			expectBuyer.LastName).Return(nil, fmt.Errorf("buyer already registered: %d", expectBuyer.CardNumberId))
+			expectBuyer.LastName).Return(nil, fmt.Errorf("buyer already registered %d", expectBuyer.CardNumberId)).Once()
 
 		controller := controllers.NewBuyer(service)
 		requestBody, _ := json.Marshal(body)
 
 		r := SetUpRouter()
-		r.POST("/api/v1/buyers", controller.Create())
-		response := CreateRequestTest(r, "POST", "/api/v1/buyers", requestBody)
+		r.POST(ENDPOINT, controller.Create())
+		response := CreateRequestTest(r, http.MethodPost, ENDPOINT, requestBody)
 
-		assert.Equal(t, response.Code, http.StatusConflict)
+		assert.Equal(t, http.StatusConflict, response.Code)
+
 	})
 }
 
-func Test_Controller_GettAll(t *testing.T) {
+func Test_Controller_GetAll(t *testing.T) {
+	service := mocks.NewService(t)
 
-	body := &buyer.Buyer{
-		Id:           1,
-		CardNumberId: 402323,
-		FirstName:    "FirstNameTest",
-		LastName:     "LastNameTest",
-	}
+	t.Run("find_all: Quando a solicitação for bem-sucedida, o back-end retornará uma lista de todos os compradores existentes.", func(t *testing.T) {
 
-	t.Run("find_all", func(t *testing.T) {
-
-		service := mocks.NewService(t)
-
-		service.On("GetAll").Return([]buyer.Buyer{}, nil)
+		service.On("GetAll").Return([]buyer.Buyer{*expectBuyer}, nil).Once()
 
 		controller := controllers.NewBuyer(service)
+		requestBody, _ := json.Marshal(body)
 
 		r := SetUpRouter()
-		r.GET("/api/v1/buyers", controller.GetAll())
-		response := CreateRequestTest(r, "GET", "/api/v1/buyers", nil)
+		r.GET(ENDPOINT, controller.GetAll())
+		response := CreateRequestTest(r, http.MethodGet, ENDPOINT, requestBody)
 
 		assert.Equal(t, http.StatusOK, response.Code)
 	})
 
-	t.Run("find_by_id_non_existent", func(t *testing.T) {
-		service := mocks.NewService(t)
+	t.Run("find_all_fail: Quando a solicitação não for bem-sucedida, o back-end retornará um erro 400.", func(t *testing.T) {
 
-		service.On("GetId", int64(1)).Return(nil, fmt.Errorf("buyer with id %d not found", int64(1)))
+		service.On("GetAll").Return([]buyer.Buyer{}, fmt.Errorf("error"))
+		controller := controllers.NewBuyer(service)
+		requestBody, _ := json.Marshal(body)
 
+		r := SetUpRouter()
+		r.GET(ENDPOINT, controller.GetAll())
+		response := CreateRequestTest(r, http.MethodGet, ENDPOINT, requestBody)
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+	})
+}
+
+func Test_Controller_GetById(t *testing.T) {
+	service := mocks.NewService(t)
+
+	t.Run("find_by_id_existent: Quando a solicitação for bem-sucedida, o back-end retornará uma lista de todos os compradores existentes.", func(t *testing.T) {
+
+		service.On("GetId", int64(1)).Return(body, nil).Once()
+
+		controller := controllers.NewBuyer(service)
+		requestBody, _ := json.Marshal(body)
+
+		r := SetUpRouter()
+		r.GET(ENDPOINT+"/:id", controller.GetId())
+		response := CreateRequestTest(r, http.MethodGet, ENDPOINT+"/1", requestBody)
+		assert.Equal(t, http.StatusOK, response.Code)
+
+	})
+
+	t.Run("find_by_id_inexistent: Quando o funcionário não existir, um código 404 será retornado", func(t *testing.T) {
+
+		service.On("GetId", int64(1)).Return(nil, fmt.Errorf("buyer not found")).Once()
 		controller := controllers.NewBuyer(service)
 
 		r := SetUpRouter()
-		r.GET("/api/v1/buyers/:id", controller.GetId())
-		response := CreateRequestTest(r, "GET", "/api/v1/buyers/1", nil)
-
+		r.GET(ENDPOINT+"/:id", controller.GetId())
+		response := CreateRequestTest(r, http.MethodGet, ENDPOINT+"/1", []byte{})
 		assert.Equal(t, http.StatusNotFound, response.Code)
 	})
 
-	t.Run("find_by_id_existent", func(t *testing.T) {
-		service := mocks.NewService(t)
-
-		service.On("GetId", int64(1)).Return(body, nil)
-
+	t.Run("find_by_id_parse_error: Quando a solicitação não for bem-sucedida, o back-end retornará um erro 400.", func(t *testing.T) {
 		controller := controllers.NewBuyer(service)
 
 		r := SetUpRouter()
-		r.GET("/api/v1/buyers/:id", controller.GetId())
-		response := CreateRequestTest(r, "GET", "/api/v1/buyers/1", nil)
+		r.GET(ENDPOINT+"/:id", controller.GetId())
+		response := CreateRequestTest(r, http.MethodGet, ENDPOINT+"/idInvalido", []byte{})
 
-		assert.Equal(t, http.StatusOK, response.Code)
+		assert.Equal(t, http.StatusBadRequest, response.Code)
 	})
 }
 
 func Test_Controller_Update(t *testing.T) {
+	service := mocks.NewService(t)
 
 	updateBody := &buyer.Buyer{
 		Id:           1,
-		CardNumberId: 402323,
-		LastName:     "LastNameTest",
+		CardNumberId: 402324,
+		LastName:     "LastNameTest 2",
 	}
 
-	t.Run("update_ok", func(t *testing.T) {
+	t.Run("update_ok: Quando a atualização dos dados for bem sucedida, o comprador será devolvido com as informações atualizadas juntamente com um código 200", func(t *testing.T) {
 
-		service := mocks.NewService(t)
-
-		service.On("Update", int64(1), updateBody.CardNumberId, updateBody.LastName).Return(updateBody, nil)
+		service.On("Update", int64(1), updateBody.CardNumberId, updateBody.LastName).Return(updateBody, nil).Once()
 
 		controller := controllers.NewBuyer(service)
 		requestBody, _ := json.Marshal(updateBody)
 
 		r := SetUpRouter()
-		r.PATCH("/api/v1/buyers/:id", controller.UpdateCardNumberLastName())
-		response := CreateRequestTest(r, "PATCH", "/api/v1/buyers/1", requestBody)
+		r.PATCH(ENDPOINT+"/:id", controller.UpdateCardNumberLastName())
+		response := CreateRequestTest(r, http.MethodPatch, ENDPOINT+"/1", requestBody)
 
 		assert.Equal(t, http.StatusOK, response.Code)
 	})
 
-	t.Run("update_non_existent", func(t *testing.T) {
-
-		service := mocks.NewService(t)
-
-		service.On("Update", int64(1), updateBody.CardNumberId, updateBody.LastName).Return(nil, fmt.Errorf("buyer with id %d not found", int64(1)))
+	t.Run("update_non_existent: Se o comprador a ser atualizado não existir, um código 404 será devolvido.", func(t *testing.T) {
+		service.On("Update", int64(1), updateBody.CardNumberId, updateBody.LastName).Return(nil, fmt.Errorf("buyer with id %d not found", int64(1))).Once()
 
 		controller := controllers.NewBuyer(service)
 		requestBody, _ := json.Marshal(updateBody)
 
 		r := SetUpRouter()
 		r.PATCH("/api/v1/buyers/:id", controller.UpdateCardNumberLastName())
-		response := CreateRequestTest(r, "PATCH", "/api/v1/buyers/1", requestBody)
+		response := CreateRequestTest(r, http.MethodPatch, "/api/v1/buyers/1", requestBody)
 
 		assert.Equal(t, http.StatusNotFound, response.Code)
+	})
+
+	t.Run("update_id_parse_error: Quando a solicitação não for bem-sucedida, o back-end retornará um erro 400.", func(t *testing.T) {
+		controller := controllers.NewBuyer(service)
+		r := SetUpRouter()
+		r.PATCH(ENDPOINT+"/:id", controller.UpdateCardNumberLastName())
+		response := CreateRequestTest(r, http.MethodPatch, ENDPOINT+"/idInvalido", []byte{})
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
 	})
 }
-
 func Test_Controller_Delete(t *testing.T) {
+	service := mocks.NewService(t)
 
-	t.Run("delete_non_existent", func(t *testing.T) {
+	t.Run("delete_non_existent: Quando o comprador não existir, um código 404 será devolvido", func(t *testing.T) {
 
-		service := mocks.NewService(t)
-
-		service.On("Delete", int64(1)).Return(fmt.Errorf("buyer with id %d not found", int64(1)))
+		service.On("Delete", int64(1)).Return(fmt.Errorf("buyer with id not found")).Once()
 
 		controller := controllers.NewBuyer(service)
 
 		r := SetUpRouter()
-		r.DELETE("/api/v1/buyers/:id", controller.DeleteBuyer())
-		response := CreateRequestTest(r, "DELETE", "/api/v1/buyers/1", nil)
+
+		r.DELETE(ENDPOINT+"/:id", controller.DeleteBuyer())
+		response := CreateRequestTest(r, http.MethodDelete, ENDPOINT+"/1", []byte{})
 
 		assert.Equal(t, http.StatusNotFound, response.Code)
 	})
 
-	t.Run("delete_ok", func(t *testing.T) {
+	t.Run("delete_ok: Quando a exclusão for bem-sucedida, um código 204 será retornado.", func(t *testing.T) {
 
-		service := mocks.NewService(t)
-
-		service.On("Delete", int64(1)).Return(nil)
+		service.On("Delete", int64(1)).Return(nil).Once()
 
 		controller := controllers.NewBuyer(service)
 
 		r := SetUpRouter()
-		r.DELETE("/api/v1/buyers/:id", controller.DeleteBuyer())
-		response := CreateRequestTest(r, "DELETE", "/api/v1/buyers/1", nil)
+		r.DELETE(ENDPOINT+"/:id", controller.DeleteBuyer())
+		response := CreateRequestTest(r, http.MethodDelete, ENDPOINT+"/1", []byte{})
 
 		assert.Equal(t, http.StatusNoContent, response.Code)
+	})
+
+	t.Run("delete_id_parse_error: Quando o id do comprador for inválido, um código 400 será devolvido", func(t *testing.T) {
+
+		controller := controllers.NewBuyer(service)
+		r := SetUpRouter()
+		r.DELETE(ENDPOINT+"/:id", controller.DeleteBuyer())
+
+		response := CreateRequestTest(r, http.MethodDelete, ENDPOINT+"/idInvalido", []byte{})
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
 	})
 }
