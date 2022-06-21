649e2f247c157fb7aaaf9e03ec278f9a6fb604b5
zurcje <jwdcruz@gmail.com>
2022-06-21 11:06:25 -0300

    settings after review

--- cmd/server/controllers/buyer_test.go
+++ cmd/server/controllers/buyer_test.go
@@ -20,9 +20,9 @@ func SetUpRouter() *gin.Engine {
 	return router
 }
 
-func CreateRequestTest(router *gin.Engine, method string, url string, body []byte) *httptest.ResponseRecorder {
+func ExecuteTestRequest(router *gin.Engine, method string, path string, body []byte) *httptest.ResponseRecorder {
 
-	request := httptest.NewRequest(method, url, bytes.NewBuffer(body))
+	request := httptest.NewRequest(method, path, bytes.NewBuffer(body))
 	response := httptest.NewRecorder()
 
 	router.ServeHTTP(response, request)
@@ -50,17 +50,20 @@ func Test_Controller_Create(t *testing.T) {
 
 	t.Run("create_ok: when data entry is successful, should return code 201.", func(t *testing.T) {
 
-		service.On("Create",
-			expectBuyer.CardNumberId,
-			expectBuyer.FirstName,
-			expectBuyer.LastName).Return(expectBuyer, nil).Once()
+		service.
+			On("Create",
+				expectBuyer.CardNumberId,
+				expectBuyer.FirstName,
+				expectBuyer.LastName).
+			Return(expectBuyer, nil).
+			Once()
 
 		controller := controllers.NewBuyer(service)
 		requestBody, _ := json.Marshal(body)
 
 		r := SetUpRouter()
 		r.POST(ENDPOINT, controller.Create())
-		response := CreateRequestTest(r, http.MethodPost, ENDPOINT, requestBody)
+		response := ExecuteTestRequest(r, http.MethodPost, ENDPOINT, requestBody)
 
 		assert.Equal(t, http.StatusCreated, response.Code)
 
@@ -73,24 +76,27 @@ func Test_Controller_Create(t *testing.T) {
 
 		r := SetUpRouter()
 		r.POST(ENDPOINT, controller.Create())
-		response := CreateRequestTest(r, http.MethodPost, ENDPOINT, []byte{})
+		response := ExecuteTestRequest(r, http.MethodPost, ENDPOINT, []byte{})
 
 		assert.Equal(t, http.StatusUnprocessableEntity, response.Code)
 	})
 
 	t.Run("create_conflict: when the card_number already exists, should return code 409.", func(t *testing.T) {
 
-		service.On("Create",
-			expectBuyer.CardNumberId,
-			expectBuyer.FirstName,
-			expectBuyer.LastName).Return(nil, fmt.Errorf("buyer already registered %d", expectBuyer.CardNumberId)).Once()
+		service.
+			On("Create",
+				expectBuyer.CardNumberId,
+				expectBuyer.FirstName,
+				expectBuyer.LastName).
+			Return(nil, fmt.Errorf("buyer already registered %d", expectBuyer.CardNumberId)).
+			Once()
 
 		controller := controllers.NewBuyer(service)
 		requestBody, _ := json.Marshal(body)
 
 		r := SetUpRouter()
 		r.POST(ENDPOINT, controller.Create())
-		response := CreateRequestTest(r, http.MethodPost, ENDPOINT, requestBody)
+		response := ExecuteTestRequest(r, http.MethodPost, ENDPOINT, requestBody)
 
 		assert.Equal(t, http.StatusConflict, response.Code)
 
@@ -102,14 +108,17 @@ func Test_Controller_GetAll(t *testing.T) {
 
 	t.Run("find_all: when data entry is successful, should return code 200.", func(t *testing.T) {
 
-		service.On("GetAll").Return([]buyer.Buyer{*expectBuyer}, nil).Once()
+		service.
+			On("GetAll").
+			Return([]buyer.Buyer{*expectBuyer}, nil).
+			Once()
 
 		controller := controllers.NewBuyer(service)
 		requestBody, _ := json.Marshal(body)
 
 		r := SetUpRouter()
 		r.GET(ENDPOINT, controller.GetAll())
-		response := CreateRequestTest(r, http.MethodGet, ENDPOINT, requestBody)
+		response := ExecuteTestRequest(r, http.MethodGet, ENDPOINT, requestBody)
 
 		assert.Equal(t, http.StatusOK, response.Code)
 
@@ -118,13 +127,17 @@ func Test_Controller_GetAll(t *testing.T) {
 
 	t.Run("find_all_fail: when GetAll fail, should return code 400.", func(t *testing.T) {
 
-		service.On("GetAll").Return([]buyer.Buyer{}, fmt.Errorf("error"))
+		service.
+			On("GetAll").
+			Return([]buyer.Buyer{}, fmt.Errorf("error")).
+			Once()
+
 		controller := controllers.NewBuyer(service)
 		requestBody, _ := json.Marshal(body)
 
 		r := SetUpRouter()
 		r.GET(ENDPOINT, controller.GetAll())
-		response := CreateRequestTest(r, http.MethodGet, ENDPOINT, requestBody)
+		response := ExecuteTestRequest(r, http.MethodGet, ENDPOINT, requestBody)
 
 		assert.Equal(t, http.StatusBadRequest, response.Code)
 
@@ -136,14 +149,17 @@ func Test_Controller_GetById(t *testing.T) {
 
 	t.Run("find_by_id_existent: when the request is successful, should return code 200", func(t *testing.T) {
 
-		service.On("GetId", int64(1)).Return(body, nil).Once()
+		service.
+			On("GetId", int64(1)).
+			Return(body, nil).
+			Once()
 
 		controller := controllers.NewBuyer(service)
 		requestBody, _ := json.Marshal(body)
 
 		r := SetUpRouter()
 		r.GET(ENDPOINT+"/:id", controller.GetId())
-		response := CreateRequestTest(r, http.MethodGet, ENDPOINT+"/1", requestBody)
+		response := ExecuteTestRequest(r, http.MethodGet, ENDPOINT+"/1", requestBody)
 		assert.Equal(t, http.StatusOK, response.Code)
 		assert.JSONEq(t, "{\"data\":{\"id\":0,\"card_number_id\":402323,\"first_name\":\"FirstNameTest\",\"last_name\":\"LastNameTest\"}}", response.Body.String())
 
@@ -151,12 +167,15 @@ func Test_Controller_GetById(t *testing.T) {
 
 	t.Run("find_by_id_inexistent: when the buyer does not exist, should return code 404", func(t *testing.T) {
 
-		service.On("GetId", int64(1)).Return(nil, fmt.Errorf("buyer not found")).Once()
+		service.
+			On("GetId", int64(1)).
+			Return(nil, fmt.Errorf("buyer not found")).
+			Once()
 		controller := controllers.NewBuyer(service)
 
 		r := SetUpRouter()
 		r.GET(ENDPOINT+"/:id", controller.GetId())
-		response := CreateRequestTest(r, http.MethodGet, ENDPOINT+"/1", []byte{})
+		response := ExecuteTestRequest(r, http.MethodGet, ENDPOINT+"/1", []byte{})
 		assert.Equal(t, http.StatusNotFound, response.Code)
 
 	})
@@ -166,7 +185,7 @@ func Test_Controller_GetById(t *testing.T) {
 
 		r := SetUpRouter()
 		r.GET(ENDPOINT+"/:id", controller.GetId())
-		response := CreateRequestTest(r, http.MethodGet, ENDPOINT+"/idInvalido", []byte{})
+		response := ExecuteTestRequest(r, http.MethodGet, ENDPOINT+"/idInvalido", []byte{})
 
 		assert.Equal(t, http.StatusBadRequest, response.Code)
 	})
@@ -183,28 +202,34 @@ func Test_Controller_Update(t *testing.T) {
 
 	t.Run("update_ok: when the request is successful, should return code 200", func(t *testing.T) {
 
-		service.On("Update", int64(1), updateBody.CardNumberId, updateBody.LastName).Return(updateBody, nil).Once()
+		service.
+			On("Update", int64(1), updateBody.CardNumberId, updateBody.LastName).
+			Return(updateBody, nil).
+			Once()
 
 		controller := controllers.NewBuyer(service)
 		requestBody, _ := json.Marshal(updateBody)
 
 		r := SetUpRouter()
 		r.PATCH(ENDPOINT+"/:id", controller.UpdateCardNumberLastName())
-		response := CreateRequestTest(r, http.MethodPatch, ENDPOINT+"/1", requestBody)
+		response := ExecuteTestRequest(r, http.MethodPatch, ENDPOINT+"/1", requestBody)
 
 		assert.Equal(t, http.StatusOK, response.Code)
 		assert.JSONEq(t, "{\"data\":{\"id\":1,\"card_number_id\":402324,\"first_name\":\"\",\"last_name\":\"LastNameTest 2\"}}", response.Body.String())
 	})
 
 	t.Run("update_non_existent: when the buyer does not exist, should return code 404.", func(t *testing.T) {
-		service.On("Update", int64(1), updateBody.CardNumberId, updateBody.LastName).Return(nil, fmt.Errorf("buyer with id %d not found", int64(1))).Once()
+		service.
+			On("Update", int64(1), updateBody.CardNumberId, updateBody.LastName).
+			Return(nil, fmt.Errorf("buyer with id %d not found", int64(1))).
+			Once()
 
 		controller := controllers.NewBuyer(service)
 		requestBody, _ := json.Marshal(updateBody)
 
 		r := SetUpRouter()
 		r.PATCH("/api/v1/buyers/:id", controller.UpdateCardNumberLastName())
-		response := CreateRequestTest(r, http.MethodPatch, "/api/v1/buyers/1", requestBody)
+		response := ExecuteTestRequest(r, http.MethodPatch, "/api/v1/buyers/1", requestBody)
 
 		assert.Equal(t, http.StatusNotFound, response.Code)
 	})
@@ -213,7 +238,17 @@ func Test_Controller_Update(t *testing.T) {
 		controller := controllers.NewBuyer(service)
 		r := SetUpRouter()
 		r.PATCH(ENDPOINT+"/:id", controller.UpdateCardNumberLastName())
-		response := CreateRequestTest(r, http.MethodPatch, ENDPOINT+"/idInvalido", []byte{})
+		response := ExecuteTestRequest(r, http.MethodPatch, ENDPOINT+"/idInvalido", []byte{})
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+	})
+
+	t.Run("update_body_invalid: when the request body is not valid json, should return code 400.", func(t *testing.T) {
+		controller := controllers.NewBuyer(service)
+
+		r := SetUpRouter()
+		r.PATCH(ENDPOINT+"/:id", controller.UpdateCardNumberLastName())
+		response := ExecuteTestRequest(r, http.MethodPatch, ENDPOINT+"/1", nil)
 
 		assert.Equal(t, http.StatusBadRequest, response.Code)
 	})
@@ -223,27 +258,33 @@ func Test_Controller_Delete(t *testing.T) {
 
 	t.Run("delete_non_existent: when the buyer does not exist, should return code 404", func(t *testing.T) {
 
-		service.On("Delete", int64(1)).Return(fmt.Errorf("buyer with id not found")).Once()
+		service.
+			On("Delete", int64(1)).
+			Return(fmt.Errorf("buyer with id not found")).
+			Once()
 
 		controller := controllers.NewBuyer(service)
 
 		r := SetUpRouter()
 
 		r.DELETE(ENDPOINT+"/:id", controller.DeleteBuyer())
-		response := CreateRequestTest(r, http.MethodDelete, ENDPOINT+"/1", []byte{})
+		response := ExecuteTestRequest(r, http.MethodDelete, ENDPOINT+"/1", []byte{})
 
 		assert.Equal(t, http.StatusNotFound, response.Code)
 	})
 
 	t.Run("delete_ok: when the request is successful, should return code 204.", func(t *testing.T) {
 
-		service.On("Delete", int64(1)).Return(nil).Once()
+		service.
+			On("Delete", int64(1)).
+			Return(nil).
+			Once()
 
 		controller := controllers.NewBuyer(service)
 
 		r := SetUpRouter()
 		r.DELETE(ENDPOINT+"/:id", controller.DeleteBuyer())
-		response := CreateRequestTest(r, http.MethodDelete, ENDPOINT+"/1", []byte{})
+		response := ExecuteTestRequest(r, http.MethodDelete, ENDPOINT+"/1", []byte{})
 
 		assert.Equal(t, http.StatusNoContent, response.Code)
 	})
@@ -254,7 +295,7 @@ func Test_Controller_Delete(t *testing.T) {
 		r := SetUpRouter()
 		r.DELETE(ENDPOINT+"/:id", controller.DeleteBuyer())
 
-		response := CreateRequestTest(r, http.MethodDelete, ENDPOINT+"/idInvalido", []byte{})
+		response := ExecuteTestRequest(r, http.MethodDelete, ENDPOINT+"/idInvalido", []byte{})
 
 		assert.Equal(t, http.StatusBadRequest, response.Code)
 	})
