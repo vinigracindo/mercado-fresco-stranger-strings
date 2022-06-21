25cf56be4b51706e241f5b5eed212c2a9f697d04
zurcje <jwdcruz@gmail.com>
2022-06-21 09:45:10 -0300

    settings after review

--- internal/domains/buyer/service_test.go
+++ internal/domains/buyer/service_test.go
@@ -9,12 +9,27 @@ import (
 	"github.com/vinigracindo/mercado-fresco-stranger-strings/internal/domains/buyer/mocks"
 )
 
-func TestService_Create(t *testing.T) {
-	expectBuyer := &buyer.Buyer{
+var expectBuyer = &buyer.Buyer{
+	Id:           1,
+	CardNumberId: 402323,
+	FirstName:    "FirstNameTest",
+	LastName:     "LastNameTest",
+}
+
+var expectBuyerList = []buyer.Buyer{
+	{
 		CardNumberId: 402323,
 		FirstName:    "FirstNameTest",
 		LastName:     "LastNameTest",
-	}
+	},
+	{
+		CardNumberId: 402300,
+		FirstName:    "FirstNameTest 2",
+		LastName:     "LastNameTestTest2",
+	},
+}
+
+func TestService_Create(t *testing.T) {
 
 	t.Run("crete_ok: Se contiver os campos necessários, será criado", func(t *testing.T) {
 		repo := mocks.NewRepository(t)
@@ -46,18 +61,7 @@ func TestService_Create(t *testing.T) {
 }
 
 func TestService_GetAll(t *testing.T) {
-	expectBuyerList := []buyer.Buyer{
-		{
-			CardNumberId: 402323,
-			FirstName:    "FirstNameTest",
-			LastName:     "LastNameTest",
-		},
-		{
-			CardNumberId: 402300,
-			FirstName:    "FirstNameTest 2",
-			LastName:     "LastNameTestTest2",
-		},
-	}
+
 	t.Run("find_all: Se a lista tiver elementos, retornará o número total de elementos", func(t *testing.T) {
 		repo := mocks.NewRepository(t)
 
@@ -86,33 +90,21 @@ func TestService_GetId(t *testing.T) {
 	})
 
 	t.Run("find_by_id_existent: Se o elemento procurado por id existir, ele retornará as informações do elemento solicitado", func(t *testing.T) {
-		expectBuyer := buyer.Buyer{
-
-			CardNumberId: 402323,
-			FirstName:    "FirstNameTest",
-			LastName:     "LastNameTest",
-		}
 
 		repo := mocks.NewRepository(t)
 
-		repo.On("GetId", int64(1)).Return(&expectBuyer, nil)
+		repo.On("GetId", int64(1)).Return(expectBuyer, nil)
 		service := buyer.NewService(repo)
 
 		buyer, err := service.GetId(int64(1))
 
 		assert.Nil(t, err)
-		assert.Equal(t, buyer, &expectBuyer)
+		assert.Equal(t, buyer, expectBuyer)
 
 	})
 }
 
 func TestService_Update(t *testing.T) {
-	expectBuyer := &buyer.Buyer{
-		Id:           1,
-		CardNumberId: 402323,
-		FirstName:    "FirstNameTest",
-		LastName:     "LastNameTest",
-	}
 
 	t.Run("update_existent: Quando a atualização dos dados for bem sucedida, o comprador será devolvido com as informações atualizadas", func(t *testing.T) {
 		repo := mocks.NewRepository(t)
