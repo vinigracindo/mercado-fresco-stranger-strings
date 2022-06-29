4d9cf0d507a27fa8585c753aef37c1cdd72369c1
Vinnicyus Gracindo <vinnicyus.gracindo@mercadolivre.com>
2022-06-29 17:55:01 -0300

    add getbyid employee repository tests

--- internal/employees/repository/EmployeeRepository_test.go
+++ internal/employees/repository/EmployeeRepository_test.go
@@ -84,3 +84,47 @@ func TestEmployeeRepository_GetAll(t *testing.T) {
 		assert.Empty(t, result)
 	})
 }
+
+func TestEmployeeRepository_GetById(t *testing.T) {
+	ctx := context.Background()
+
+	expectedEmployee := domain.Employee{
+		Id:           1,
+		CardNumberId: "123456",
+		FirstName:    "John",
+		LastName:     "Doe",
+		WarehouseId:  1,
+	}
+
+	t.Run("should return employee by id", func(t *testing.T) {
+		db, mock, err := sqlmock.New()
+		assert.NoError(t, err)
+		defer db.Close()
+
+		rows := sqlmock.NewRows([]string{"id", "card_number_id", "first_name", "last_name", "warehouse_id"}).AddRow(expectedEmployee.Id, expectedEmployee.CardNumberId, expectedEmployee.FirstName, expectedEmployee.LastName, expectedEmployee.WarehouseId)
+
+		employeeRepository := repository.NewMariaDBEmployeeRepository(db)
+
+		mock.ExpectQuery(repository.SQLFindEmployeeByID).WithArgs(expectedEmployee.Id).WillReturnRows(rows)
+
+		result, err := employeeRepository.GetById(ctx, expectedEmployee.Id)
+
+		assert.Nil(t, err)
+		assert.Equal(t, expectedEmployee, *result)
+	})
+
+	t.Run("should return error when query fails", func(t *testing.T) {
+		db, mock, err := sqlmock.New()
+		assert.NoError(t, err)
+		defer db.Close()
+
+		employeeRepository := repository.NewMariaDBEmployeeRepository(db)
+
+		mock.ExpectQuery(repository.SQLFindEmployeeByID).WithArgs(expectedEmployee.Id).WillReturnError(fmt.Errorf("query error"))
+
+		result, err := employeeRepository.GetById(ctx, expectedEmployee.Id)
+
+		assert.Error(t, err)
+		assert.Empty(t, result)
+	})
+}
