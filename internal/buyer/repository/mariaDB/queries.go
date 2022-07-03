912cfa82ea2731053ac314eff2dace47cfed4145
zurcje <jwdcruz@gmail.com>
2022-07-03 20:52:39 -0300

    fix queries

--- internal/buyer/repository/mariaDB/queries.go
+++ internal/buyer/repository/mariaDB/queries.go
@@ -1,13 +1,18 @@
 package repository
 
 const (
-	SQLGetAllBuyer = "SELECT * FROM buyers"
+	SQLGetAllBuyer = `
+	SELECT id, card_number_id, first_name, last_name
+	FROM buyers`
 
-	SQLGetByIdBuyer = "SELECT * FROM buyers WHERE id=?"
+	SQLGetByIdBuyer = `
+	SELECT id, card_number_id, first_name, last_name
+	FROM buyers
+	WHERE id=?`
 
 	SQLCreateBuyer = `
     INSERT INTO
-    buyer (
+    buyers (
         card_number_id,
         first_name,
         last_name
