dc28a127ca2e62a4eb8e292406d8dc87bff41769
Vinnicyus Gracindo <vinnicyus.gracindo@mercadolivre.com>
2022-07-04 12:38:05 -0300

    add inbound orders service test

--- internal/inboud_orders/domain/InboudOrdersModel.go
+++ internal/inboud_orders/domain/InboudOrdersModel.go
@@ -5,33 +5,33 @@ import (
 	"time"
 )
 
-type InboundOrder struct {
+type InboundOrders struct {
 	Id             int64     `json:"id"`
 	OrderDate      time.Time `json:"order_date"`
-	OrderType      string    `json:"order_type"`
+	OrderNumber    string    `json:"order_number"`
 	EmployeeId     int64     `json:"employee_id"`
 	ProductBatchId int64     `json:"product_batch_id"`
 	WarehouseId    int64     `json:"warehouse_id"`
 }
 
-type InboundOrderService interface {
+type InboundOrdersService interface {
 	Create(
 		ctx context.Context,
 		orderDate time.Time,
-		orderType string,
+		orderNumber string,
 		employeeId int64,
 		productBatchId int64,
 		warehouseId int64,
-	) (InboundOrder, error)
+	) (InboundOrders, error)
 }
 
-type InboundOrderRepository interface {
+type InboundOrdersRepository interface {
 	Create(
 		ctx context.Context,
 		orderDate time.Time,
-		orderType string,
+		orderNumber string,
 		employeeId int64,
 		productBatchId int64,
 		warehouseId int64,
-	) (InboundOrder, error)
+	) (InboundOrders, error)
 }
