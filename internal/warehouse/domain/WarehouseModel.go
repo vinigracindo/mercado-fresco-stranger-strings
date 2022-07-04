ed2325af33790d3bef20feb9a4a35d11e080b0dd
Gabriel Ferreira Silva <gabrielfssilva99@gmail.com>
2022-07-04 09:35:14 -0300

    change package name

--- internal/warehouse/domain/WarehouseModel.go
+++ internal/warehouse/domain/WarehouseModel.go
@@ -1,4 +1,4 @@
-package warehouse
+package domain
 
 import "context"
 
@@ -6,7 +6,7 @@ type WarehouseModel struct {
 	Id                 int64   `json:"id"`
 	Address            string  `json:"address"`
 	Telephone          string  `json:"telephone"`
-	WarehouseCode      string  `json:"warehouse_code,require"`
+	WarehouseCode      string  `json:"warehouse_code"`
 	MinimunCapacity    int64   `json:"minimun_capacity"`
 	MinimunTemperature float64 `json:"minimun_temperature"`
 	LocalityID         int64   `json:"locality_id"`
