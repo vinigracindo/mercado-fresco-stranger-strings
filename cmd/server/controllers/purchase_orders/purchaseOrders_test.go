d8687da44b4d42093f91a1b00bd64c26ba1dc900
zurcje <jwdcruz@gmail.com>
2022-07-06 14:04:23 -0300

    controller test - create nok

--- cmd/server/controllers/purchase_orders/purchaseOrders_test.go
+++ cmd/server/controllers/purchase_orders/purchaseOrders_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	controllers "github.com/vinigracindo/mercado-fresco-stranger-strings/cmd/server/controllers/purchase_orders"
+	buyerDomain "github.com/vinigracindo/mercado-fresco-stranger-strings/internal/buyer/domain"
 	"github.com/vinigracindo/mercado-fresco-stranger-strings/internal/purchase_orders/domain"
 	"github.com/vinigracindo/mercado-fresco-stranger-strings/internal/purchase_orders/domain/mocks"
 	"github.com/vinigracindo/mercado-fresco-stranger-strings/pkg/testutil"
@@ -68,4 +69,39 @@ func TestPurchaseOrdersController_Create(t *testing.T) {
 
 	})
 
+	t.Run("create_not_found: when the buyer id is not found, should return code 409", func(t *testing.T) {
+
+		service.On("Create",
+			context.TODO(),
+			bodyPurchaseOrders.OrderNumber,
+			bodyPurchaseOrders.OrderDate,
+			bodyPurchaseOrders.TrackingCode,
+			bodyPurchaseOrders.BuyerId,
+			bodyPurchaseOrders.ProductRecordId,
+			bodyPurchaseOrders.OrderStatusId,
+		).Return(nil, buyerDomain.ErrIDNotFound).Once()
+
+		requestBody, _ := json.Marshal(expectedPurchaseOrders)
+		response := testutil.ExecuteTestRequest(r, http.MethodPost, EndpointPurchaseOrders, requestBody)
+
+		assert.Equal(t, http.StatusConflict, response.Code)
+
+	})
+
+	t.Run("create_fail_invalid_time_parse: when the request body is invalid, should return code 400", func(t *testing.T) {
+
+		invalidDatePurchaseOrders := controllers.PurchaseOrdersCreate{
+			OrderNumber:     "order#1",
+			OrderDate:       "invalid date",
+			TrackingCode:    "abscf123",
+			BuyerId:         1,
+			ProductRecordId: 1,
+			OrderStatusId:   1,
+		}
+
+		requestBody, _ := json.Marshal(invalidDatePurchaseOrders)
+		response := testutil.ExecuteTestRequest(r, http.MethodPost, EndpointPurchaseOrders, requestBody)
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+	})
 }
