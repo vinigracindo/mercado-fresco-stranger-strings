74c5e4dd3f9af11cebfa96f7d8e6dad874e4491b
zurcje <jwdcruz@gmail.com>
2022-07-06 13:39:25 -0300

    controller test - create

--- cmd/server/controllers/purchase_orders/purchaseOrders_test.go
+++ cmd/server/controllers/purchase_orders/purchaseOrders_test.go
@@ -15,50 +15,56 @@ import (
 )
 
 var EndpointPurchaseOrders = "/api/v1/purchaseOrders"
-var ctx = context.Background()
-var orderDateNow = time.Date(2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
-
-var expectedPurchaseOrders = &domain.PurchaseOrders{
-	Id:              1,
-	OrderNumber:     "order#1",
-	OrderDate:       orderDateNow,
-	TrackingCode:    "abscf123",
-	BuyerId:         1,
-	ProductRecordId: 1,
-	OrderStatusId:   1,
-}
-
-var bodyPurchaseOrders = &domain.PurchaseOrders{
-	OrderNumber:     "order#1",
-	OrderDate:       orderDateNow,
-	TrackingCode:    "abscf123",
-	BuyerId:         1,
-	ProductRecordId: 1,
-	OrderStatusId:   1,
-}
 
 func TestPurchaseOrdersController_Create(t *testing.T) {
+
+	var expectedPurchaseOrders = controllers.PurchaseOrdersCreate{
+		OrderNumber:     "order#1",
+		OrderDate:       "2022-06-06",
+		TrackingCode:    "abscf123",
+		BuyerId:         1,
+		ProductRecordId: 1,
+		OrderStatusId:   1,
+	}
+
+	orderDateNow, _ := time.Parse("2006-01-02", expectedPurchaseOrders.OrderDate)
+
+	var bodyPurchaseOrders = &domain.PurchaseOrders{
+		OrderNumber:     "order#1",
+		OrderDate:       orderDateNow,
+		TrackingCode:    "abscf123",
+		BuyerId:         1,
+		ProductRecordId: 1,
+		OrderStatusId:   1,
+	}
 	service := mocks.NewPurchaseOrdersService(t)
+	controller := controllers.NewPurchaseOrdersController(service)
+	r := testutil.SetUpRouter()
+	r.POST(EndpointPurchaseOrders, controller.Create())
 
 	t.Run("create_ok: when data entry is successful, should return code 201.", func(t *testing.T) {
 		service.On("Create",
-			ctx,
-			expectedPurchaseOrders.OrderNumber,
-			expectedPurchaseOrders.OrderDate,
-			expectedPurchaseOrders.TrackingCode,
-			expectedPurchaseOrders.BuyerId,
-			expectedPurchaseOrders.ProductRecordId,
-			expectedPurchaseOrders.OrderStatusId,
-		).Return(expectedPurchaseOrders, nil).Once()
+			context.TODO(),
+			bodyPurchaseOrders.OrderNumber,
+			bodyPurchaseOrders.OrderDate,
+			bodyPurchaseOrders.TrackingCode,
+			bodyPurchaseOrders.BuyerId,
+			bodyPurchaseOrders.ProductRecordId,
+			bodyPurchaseOrders.OrderStatusId,
+		).Return(bodyPurchaseOrders, nil).Once()
 
-		controller := controllers.NewPurchaseOrdersController(service)
-		requestBody, _ := json.Marshal(bodyPurchaseOrders)
-
-		r := testutil.SetUpRouter()
-		r.POST(EndpointPurchaseOrders, controller.Create())
+		requestBody, _ := json.Marshal(expectedPurchaseOrders)
 		response := testutil.ExecuteTestRequest(r, http.MethodPost, EndpointPurchaseOrders, requestBody)
 
 		assert.Equal(t, http.StatusCreated, response.Code)
+	})
+
+	t.Run("create_fail_invalid_json: when the JSON does not contain the required fields, should return code 422", func(t *testing.T) {
+
+		requestBody, _ := json.Marshal([]byte{})
+		response := testutil.ExecuteTestRequest(r, http.MethodPost, EndpointPurchaseOrders, requestBody)
+
+		assert.Equal(t, http.StatusUnprocessableEntity, response.Code)
 
 	})
 
