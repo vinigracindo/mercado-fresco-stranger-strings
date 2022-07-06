7ebe30ae54e82131d2c36ae2f9746a35ef426197
zurcje <jwdcruz@gmail.com>
2022-07-05 22:12:18 -0300

    fix date - PurchaseOrders

--- internal/purchase_orders/domain/PurchaseOrdersModel.go
+++ internal/purchase_orders/domain/PurchaseOrdersModel.go
@@ -1,21 +1,24 @@
 package domain
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type PurchaseOrders struct {
-	Id              int64  `json:"id"`
-	OrderNumber     string `json:"order_number"`
-	OrderDate       string `json:"order_date"`
-	TrackingCode    string `json:"tracking_code"`
-	BuyerId         int64  `json:"buyer_id"`
-	ProductRecordId int64  `json:"product_record_id"`
-	OrderStatusId   int64  `json:"order_status_id"`
+	Id              int64     `json:"id"`
+	OrderNumber     string    `json:"order_number"`
+	OrderDate       time.Time `json:"order_date"`
+	TrackingCode    string    `json:"tracking_code"`
+	BuyerId         int64     `json:"buyer_id"`
+	ProductRecordId int64     `json:"product_record_id"`
+	OrderStatusId   int64     `json:"order_status_id"`
 }
 
 type PurchaseOrdersRepository interface {
-	Create(ctx context.Context, OrderNumber, OrderDate, TrackingCode string, BuyerId, ProductRecordId, OrderStatusId int64) (*PurchaseOrders, error)
+	Create(ctx context.Context, OrderNumber string, OrderDate time.Time, TrackingCode string, BuyerId, ProductRecordId, OrderStatusId int64) (*PurchaseOrders, error)
 }
 
 type PurchaseOrdersService interface {
-	Create(ctx context.Context, OrderNumber, OrderDate, TrackingCode string, BuyerId, ProductRecordId, OrderStatusId int64) (*PurchaseOrders, error)
+	Create(ctx context.Context, OrderNumber string, OrderDate time.Time, TrackingCode string, BuyerId, ProductRecordId, OrderStatusId int64) (*PurchaseOrders, error)
 }
