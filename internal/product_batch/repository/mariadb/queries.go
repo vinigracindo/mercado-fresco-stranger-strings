736684d24d1650bc9874ecfdf96d0a6eb8a3c638
Gabriel Ferreira Silva <gabrielfssilva99@gmail.com>
2022-07-14 18:16:10 -0300

    fix: fix INSERT INTO product_batches query

--- internal/product_batch/repository/mariadb/queries.go
+++ internal/product_batch/repository/mariadb/queries.go
@@ -2,19 +2,19 @@ package repository
 
 const (
 	SQLCreate = `
-    INSERT INTO
-    product_batches (
-        batch_number,
-        current_quantity,
-        current_temperature,
-        due_date,
-        initial_quantity,
-        manufacturing_date,
-        manufacturing_hour,
-        minumum_temperature,
-		product_id,
-		section_id
+    INSERT INTO product_batches (
+        batch_number, 
+        current_quantity, 
+        current_temperature, 
+        due_date, 
+        initial_quantity, 
+        manufacturing_date, 
+        manufacturing_hour, 
+        minimum_temperature, 
+        product_id, 
+        section_id
     )
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+
     `
 )
